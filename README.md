248209f8a81dc533af3ca2f2997100564b848cb0
yasiru2077 <yasiruudana1999@gmail.com>
2023-09-08 11:23:21 +0530

    asihhfahif

--- README.md
+++
@@ -1,2 +0,0 @@
-# laraveljtestremauth
-clothing store auth system with laravel, jetsteam and mysql
--- resources/views/components/authentication-card-logo.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="/" class="logo flex items-center justify-center">
-   <img src="image/logo.png" alt="" class="w-1/2">
-</a>
